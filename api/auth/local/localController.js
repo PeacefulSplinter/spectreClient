c9214e86804b56d8cbe3938cceb4b6c0fa43f548
Steven Williams <Steven30135@gmail.com>
2015-02-26 13:32:46 -0800

    minor fixes

--- api/auth/local/localController.js
+++ api/auth/local/localController.js
@@ -12,21 +12,18 @@ exports.index = function(req, res){
 };
 
 exports.register = function(req, res, next){
-  console.log(req.body);
-  console.log('made it to register!');
+
   var newUser = new User({username: req.body.username, password: req.body.password});
 
   newUser.save(function(err, user){
     if(err) return res.status(401).json('Registration Error: ' + err);
     // var token = jwt.sign({_id: user._id}, $config.JWT_SECRET, {expiresInMinutes: 60*5});
-    console.log('save Successful?')
-    res.status(200).send('registration complete!'); 
+    res.status(200).send(); 
   });
 };
 
 exports.login = function(req, res, next){
-  console.log(req.body);
-  console.log('made it to log in!');
+
   User.findOne({username: req.body.username}, function(err, user){
     if (err) return done(err);
     if(!user) {
@@ -36,8 +33,7 @@ exports.login = function(req, res, next){
       if(err) return res.status(401).json('Login Error: ' + err);
       // var token = jwt.sign({_id: user._id}, $config.JWT_SECRET, {expiresInMinutes: 60*5});
       // res.cookie('Token', token);
-      console.log('succssful login!');
-      res.send('Successful login');
+      res.send();
     });
   });
 };
