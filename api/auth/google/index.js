f1f932abe09d427128a281f1c306c77437f207e3
Steven Williams <Steven30135@gmail.com>
2015-02-26 14:38:39 -0800

    fixed google auth

--- api/auth/google/index.js
+++ api/auth/google/index.js
@@ -16,13 +16,13 @@ var router = express.Router();
 router.get('/google', passport.authenticate('google', {scope: 'https://www.googleapis.com/auth/plus.login'}), function (req, res) {
 });
 
-router.get('/google/callback', passport.authenticate('google', {session: false}), function (req, res) {
+router.get('/google/callback', passport.authenticate('google', { session: false }), function (req, res) {
+
   var cwd = process.cwd();
   var testFile = cwd + '/api/views/test.html';
   // var token = jwt.sign( req.user.username, $config.JWT_SECRET, {expiresInMinutes: 60*5});
   // res.cookie('Token', token);
   res.sendFile(testFile);
-  
 });
 
 module.exports = router;
