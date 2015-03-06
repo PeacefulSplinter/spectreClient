50876e12786144935c21c0267b148227aad119c0
Steven Williams <Steven30135@gmail.com>
2015-03-06 12:22:58 -0800

    Added loadOne functionality

--- api/auth/facebook/index.js
+++ api/auth/facebook/index.js
@@ -9,7 +9,6 @@ router.get('/facebook/callback', passport.authenticate('facebook', {session: fal
   var cwd = process.cwd();
   var testFile = cwd + '/api/views/test.html';
   var token = jwt.sign(req.user.username, $config.JWT_SECRET, {expiresInMinutes: 60*5});
-  console.log(token);
   res.cookie('Token', token);
   res.sendFile(testFile);
 });
--- api/user/userController.js
+++ api/user/userController.js
@@ -2,22 +2,23 @@ var User = require('./userModel.js');
 
 exports.save = function(req, res){
 	console.log(req.body);
+
+
+
 	User.findOne({'username': req.user.username }, function(err, user){
-		if (err) { return err; }
-		if (!user){ return err; }
-		console.log('before save', user.savedDashboards);
+		if (err || !user) { return err; }
 		req.body.token = null;
 		user.savedDashboards.push(req.body);
-		console.log('after save', user.savedDashboards);
 		user.save();
 		res.status(200).send();
 	});
 };
 
+
 exports.load = function(req, res) {
-	User.findOne({'username': req.user.username }, function(err, user){
+	User.findOne({'username': req.user.username }, {'password':0, 'grants':0, '__v':0, '_id':0}, function(err, user){
 		if (err || !user || user.savedDashboards.length === 0) { return err; }
-		res.status(200).json(user.savedDashboards);
+		res.status(200).json(user);
 	});
 }
 
@@ -26,7 +27,7 @@ exports.loadOne = function(req, res) {
 		if (err || !user || user.savedDashboards.length === 0) { return err; }
 		for (var i = 0; i < user.savedDashboards.length; i++) {
 			if(user.savedDashboards[i].id === req.param(':id')){
-				res.status(200).json(user.savedDashboards[i]);
+				res.status(200).json(user);
 			}
 		};
 		console.log('dashboard not found!');
