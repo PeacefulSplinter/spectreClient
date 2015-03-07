0373e00ce24caab7f86fa4102a84e43a8e1b05ec
Mike <realmike33@gmail.com>
2015-03-06 20:46:11 -0800

    working on styling for myDashboard view

--- client/app/main/dashboards/myDashboard/myDashboard.js
+++ client/app/main/dashboards/myDashboard/myDashboard.js
@@ -3,12 +3,12 @@ angular.module('Daas.main.dashboards.mydashboard', [])
 .config(function($stateProvider){
   $stateProvider
     .state('app.main.dashboards.mydashboard', {
-      url: '/mydashboard',
+      url: '/mydashboard:id',
       templateUrl: 'main/dashboards/myDashboard/myDashboard.html',
       controller: 'MyDashboardController'
     })
 })
 
-.controller('MyDashboardController', function($scope, $state, $http, Auth, $mdSidenav, $log, $mdDialog){
-
+.controller('MyDashboardController', function($scope, $stateParams, $mdSidenav, $mdDialog){
+  console.log($stateParams);
 });
